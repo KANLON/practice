b8be54bd0317b7468262df8191fd906ffaa2ec97
zhangcanlong <Canlong2015@126.com>
2019-02-14 18:10:49 +0800

    添加自定义Enable注解的说明文件

--- spring-boot-enable/src/main/java/com/kanlon/SpringBootEnableApplication.java
+++ spring-boot-enable/src/main/java/com/kanlon/SpringBootEnableApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -15,11 +16,12 @@ import java.util.Arrays;
 @SpringBootApplication
 @EnableSelfBean(packages = {"com.kanlon.entity", "com.kanlon.utils"})
 public class SpringBootEnableApplication {
+
     @Autowired
     Role abc;
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringBootEnableApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringBootEnableApplication.class, args);
         //打印出所有spring中注册的bean
         System.out.println("当前spring注册的bean类");
         String[] allBeans = context.getBeanDefinitionNames();
