77da272a784eb018cc803265228e57030112c507
kanlon <s19961234@126.com>
2019-02-14 16:57:10 +0800

    初步实现了自定义的spring boot自动注解

---
+++ spring-boot-enable/src/main/java/com/kanlon/enable/EnableSelfBean.java
@@ -0,0 +1,21 @@
+package com.kanlon.enable;
+
+import com.kanlon.config.SelfEnableAutoConfig;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * 将某个包下的所有类自动加载到spring 容器中，不管有没有注解，并打印出
+ *
+ * @author zhangcanlong
+ * @since 2019/2/14 10:42
+ **/
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import(SelfEnableAutoConfig.class)
+public @interface EnableSelfBean {
+    //传入包名
+    String[] packages() default "";
+}
---
+++ spring-boot-enable/src/main/java/com/kanlon/entity/Role.java
@@ -0,0 +1,14 @@
+package com.kanlon.entity;
+
+/**
+ * 测试自己的自动注解的实体类1
+ *
+ * @author zhangcanlong
+ * @since 2019/2/14 10:41
+ **/
+public class Role {
+    public String test(){
+     return "hello";
+    }
+
+}
