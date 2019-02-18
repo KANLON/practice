5ab1eb9312cbe74bbe22b026e169ea194299959b
kanlon <s19961234@126.com>
2019-02-18 15:22:11 +0800

    添加配置中心的refresh的功能，客户端能刷新配置

--- spring-cloud/spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/kanlon/SpringCloudConfigClientApplication.java
+++ spring-cloud/spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/kanlon/SpringCloudConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.kanlon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope   // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
