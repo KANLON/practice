db14066941e0420be1f0caf0653ee7aaca3748c4
kanlon <s19961234@126.com>
2019-09-15 16:06:02 +0800

    初步完成动态添加数据源接口

--- spring-boot-dynamic-datasource/src/main/java/com/kanlon/controller/DataSourceController.java
+++ spring-boot-dynamic-datasource/src/main/java/com/kanlon/controller/DataSourceController.java
@@ -1,11 +1,13 @@
 package com.kanlon.controller;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
 import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
 import com.kanlon.response.CommonResponse;
-import org.apache.ibatis.datasource.pooled.PooledDataSourceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
@@ -20,10 +22,10 @@ import java.util.*;
  * @author zhangcanlong
  * @since 2019-09-14
  **/
+@RestController
+@RequestMapping("datasource")
 public class DataSourceController {
 
-    @Autowired
-    private DataSource dataSource;
 
     @Autowired
     private DynamicRoutingDataSource dynamicRoutingDataSource;
@@ -33,14 +35,25 @@ public class DataSourceController {
      **/
     @PostMapping("/addDs")
     public void add(){
-        DynamicDataSourceContextHolder.push("sdf");
-        System.out.println("目前的数据源为："+dynamicRoutingDataSource.getCurrentDataSources());
-        PooledDataSourceFactory dataSourceFactory = new PooledDataSourceFactory();
-        // 设置数据源的属性
-        dataSourceFactory.setProperties(new Properties());
-        dataSourceFactory.getDataSource();
-        dynamicRoutingDataSource.addDataSource("test",dataSource);
-        System.out.println("添加后的数据源为："+dynamicRoutingDataSource.getCurrentDataSources());
+        DynamicDataSourceContextHolder.push("test_druid");
+        System.out.println("当前数据源：" + dynamicRoutingDataSource.determineDataSource());
+        Map<String, DataSource> currentDataSources = dynamicRoutingDataSource.getCurrentDataSources();
+        Set<String> currentDataSourceKey = currentDataSources.keySet();
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        druidDataSource.setUrl("jdbc:mysql://127.0.0.1:3306/dynamic_datasource?useUnicode=true&characterEncoding=utf" + "-8&useSSL=false");
+        druidDataSource.setUsername("admin");
+        druidDataSource.setPassword("admin");
+        druidDataSource.setKeepAlive(true);
+        druidDataSource.setMaxActive(10);
+        druidDataSource.setMinIdle(2);
+        druidDataSource.setValidationQuery("select 1");
+        druidDataSource.setValidationQueryTimeout(600);
+        // 如果不包含了当前数据源，才进行创建
+        if (!currentDataSourceKey.contains("test_druid")) {
+            dynamicRoutingDataSource.addDataSource("test_druid", druidDataSource);
+        }
+        System.out.println("添加后的所有数据源为：" + dynamicRoutingDataSource.getCurrentDataSources());
 
     }
 
@@ -50,6 +63,7 @@ public class DataSourceController {
      * @param ds 数据源的名称或id
      * @return com.kanlon.response.CommonResponse
      **/
+    @PostMapping("/exec-sql")
     public CommonResponse execSql(String ds) throws SQLException {
         DataSource dataSource2 = dynamicRoutingDataSource.getDataSource(ds);
         Statement statement = dataSource2.getConnection().createStatement();
